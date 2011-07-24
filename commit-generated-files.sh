6d172b6a1c09a28d8e56d2547071ccd7ae0bad7c
Nick Knowlson <nick.knw@gmail.com>
2011-07-24 13:49:07 -0700

    make it more clear which commit messages are which

--- commit-generated-files.sh
+++ commit-generated-files.sh
@@ -2,7 +2,8 @@ jekyll
 cp -rf _site/* ../nickknw.github.com/
 cd ../nickknw.github.com/
 git add .
+echo "GENERATED FILES:"
 git commit -am "`date`"
 cd ../nickknowlson.com/
-echo "site copied"
+echo "SOURCE FILES:"
 
