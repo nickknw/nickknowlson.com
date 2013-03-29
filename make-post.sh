f16408fd3aa6f3a7a610f7cacd518a2e21d7f39a
Nick Knowlson <nick@nickknowlson.com>
2013-03-29 11:01:53 -0700

    change make-post.sh to add a blank description

--- make-post.sh
+++ make-post.sh
@@ -3,6 +3,6 @@
 DATE=`date +"%Y-%m-%d"`
 NAME="$@"
 NICE_NAME=${NAME//[ .]/-}
-BOILERPLATE="---\nlayout: post\ntitle: $NAME\n---"
+BOILERPLATE="---\nlayout: post\ntitle: $NAME\ndescription:\n---"
 
 echo -e $BOILERPLATE > _posts/$DATE-$NICE_NAME.markdown
